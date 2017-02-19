f4e09ec57b06765d2bf683a1d6ab2a32b80e8d9f
julielo5 <julielo.inbox@gmail.com>
2017-02-18 16:13:04 -0800

    Changed main font to Merriweather.

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -13,6 +13,47 @@
 				<div class="site-info">
 					<!-- <a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a> -->
 				</div><!-- .site-info -->
+				<div class="footer-info">
+          <div class="contact">
+            <h3>Contact Info</h3>
+            <p>
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+                <a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+            </p>
+            <p>
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              	<a href="tel:778-456-7891">778-456-7891</a>
+            </p>
+            <p>
+              <span>
+                <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+              </span>
+              <span>
+                <a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+              </span>
+              <span>
+                <a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+              </span>
+            </p>
+          </div><!--Contact  -->
+          <div class="business-hours">
+            <h3>Business Hours</h3>
+            <p><span>Monday-Friday:</span> 9am to 5pm</p>
+            <p><span>Saturday:</span> 10am to 2pm</p>
+            <p><span>Sunday:</span> Closed</p>
+          </div><!--business-hour-->
+          <div class="text-logo">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+            	<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg" alt="Image of Inhabitent logo" />
+            </a>
+          </div><!-- text-logo -->
+        </div>
+				<div class="site-info container">
+					<p>Copyright &copy; 2016 Inhabitent</p>
+				</div>
+
+			</div><!--footer-info -->
+
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
