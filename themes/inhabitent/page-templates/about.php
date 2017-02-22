e8a16a026f88390abaad3dcb2a02bc74af4186ad
julielo5 <julielo.inbox@gmail.com>
2017-02-21 17:16:35 -0800

    Updated about.php and created page-templates folder.

---
+++ themes/inhabitent/page-templates/about.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: About Page
+*
+* @package Inhabitent Theme
+*/
+?>
+
+<?php
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<h2>Our Story</h2>
+			<?php echo CFS()->get('about_our_story'); ?>
+			<h2>Our Team</h2>
+			<?php echo CFS()->get('about_our_team'); ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
