08fb462599666efc2c541e0dd6dfa2bf73a33d42
julielo5 <julielo.inbox@gmail.com>
2017-02-23 23:01:25 -0800

    Addedproduct categories for Shop Stuff page.

--- themes/inhabitent/archive-products.php
+++ themes/inhabitent/archive-products.php
@@ -18,15 +18,26 @@ get_header(); ?>
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
+			<div class="shop-category-container">
+				<?php
+					$terms = get_terms( array(
+		    		'taxonomy' => 'product_type',
+						'orderby' => 'name',
+		    		'hide_empty' => false,
+					) );
+				?>
+
+				<?php foreach ($terms as $term) : ?>
+					<?php echo '<a class="shop-category-link" href="'.get_term_link($term).'">'.$term->name.'</a>' ?>
+				<?php endforeach; ?>
+			</div>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<div class = "shop-wrapper">
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-			</div>
+					<?php	get_template_part( 'template-parts/content' ); ?>
+				</div>
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
@@ -39,6 +50,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
