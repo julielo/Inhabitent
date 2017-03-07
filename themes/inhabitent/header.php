7b71f466337d58ee6a23815cfa7dfaaaa57e3ab8
julielo5 <julielo.inbox@gmail.com>
2017-03-07 15:34:34 -0800

    Styled search icon.

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -34,7 +34,9 @@
 							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 						</nav><!-- #site-navigation -->
-						<span class="header-search"><?php get_search_form(); ?> </span>
+						<span class="header-search">
+							<a href="#"><?php get_search_form();?></a>
+						</span>
 					</div> <!-- nav-wrapper -->
 				</div> <!-- header-container -->
 			</header><!-- #masthead -->
