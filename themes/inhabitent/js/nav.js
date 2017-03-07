632827c294be14c95aa860c40b3087811c3f7e6d
julielo5 <julielo.inbox@gmail.com>
2017-03-07 14:11:11 -0800

    Fixed search toggle feature.

--- themes/inhabitent/js/nav.js
+++ themes/inhabitent/js/nav.js
@@ -1,8 +1,8 @@
-{function($)
+(function($){
   $('.fa-search').on('click', function(event) {
     event.preventDefault();
 
-      $('.search-field').toggleClass('search-width');
+      $('.search-field').toggleClass('new-width');
       $('.search-field').focus();
     });
 })(jQuery);
